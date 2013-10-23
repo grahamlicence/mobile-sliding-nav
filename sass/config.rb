d58ea51c8fb53295b47f519da28c830b1de5a1be
Graham Licence <git@grahamlicence.co.uk>
2013-10-23 16:03:27 +0100

    Sass set up

---
+++ sass/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+require "breakpoint"
+
+# Set this to the root of your project when deployed:
+http_path = ""
+css_dir = "../assets"
+sass_dir = ""
+images_dir = "../assets"
+javascripts_dir = "../assets"
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded
+output_style = :compressed
