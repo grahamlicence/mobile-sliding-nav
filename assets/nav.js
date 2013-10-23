aac75c7566c7400861ee388238f6e783b903fb54
Graham Licence <git@grahamlicence.co.uk>
2013-10-23 16:48:46 +0100

    jQuery removed from script

--- assets/nav.js
+++ assets/nav.js
@@ -1,16 +1,38 @@
 (function () {
 	// open and close the mobile menu
+	if (!document.getElementsByClassName) {
+		return; // leave old IE alone
+	}
+
+	var addClass = function (el, classname) {
+		el.className += ' ' + classname;
+	};
+	var removeClass = function (el, classname) {
+		el.className = el.className.replace(' ' + classname, '');
+	};
+	var get = function (el) {
+		return document.getElementsByClassName(el);
+	};
+
 	var toggle = function () {
-		var $btn = $('.nav-toggle'),
-			$main = $('.main'),
-			$nav = $('.nav');
+		var btn = get('nav-toggle')[0],
+			main = get('main')[0],
+			nav = get('nav')[0],
+			open = false;
 		
-		$btn.on('click', function (e) {
-			e.preventDefault();
-			$nav.toggleClass('nav-shown');
-			$btn.toggleClass('nav-active');
-			$main.toggleClass('nav-open');
-		});
+		btn.addEventListener('click', function () {
+			if (open) {
+				removeClass(nav, 'nav-shown');
+				removeClass(btn, 'nav-active');
+				removeClass(main, 'nav-open');
+				open = false;
+			} else {
+				addClass(nav, 'nav-shown');
+				addClass(btn, 'nav-active');
+				addClass(main, 'nav-open');
+				open = true;
+			}
+		}, false);
 	};
 	return toggle();
 })();
