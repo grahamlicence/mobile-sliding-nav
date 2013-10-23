f399ef218d259acee2ac600b56ead8d5b1b06967
Graham Licence <git@grahamlicence.co.uk>
2013-10-23 15:22:04 +0100

    Content slides right on menu open

--- assets/nav.js
+++ assets/nav.js
@@ -2,12 +2,14 @@
 	// open and close the mobile menu
 	var toggle = function () {
 		var $btn = $('.nav-toggle'),
+			$main = $('.main'),
 			$nav = $('.nav');
 		
 		$btn.on('click', function (e) {
 			e.preventDefault();
 			$nav.toggleClass('nav-shown');
 			$btn.toggleClass('nav-active');
+			$main.toggleClass('nav-open');
 		});
 	};
 	return toggle();
