869fa5056a14958b6b4cbf259b5d9e0ae8fadbfb
Graham Licence <git@grahamlicence.co.uk>
2013-11-21 14:15:42 +0000

    Layout Sass split out and dummy links added

--- assets/nav.js
+++ assets/nav.js
@@ -18,23 +18,57 @@
 		var btn = get('nav-toggle')[0],
 			contentSection = get('nav__content-section')[0],
 			body = document.getElementsByTagName('body')[0],
-			main = get('main')[0],
 			nav = get('nav')[0],
-			open = false;
+			navClosing = false,
+			navOpen = false;
 		
-		btn.addEventListener('click', function () {
-			if (open) {
+		btn.addEventListener('click', function (e) {
+			e.preventDefault();
+			if (navOpen) {
+				if (navClosing) {
+					// navigation is closing, do nothing until the animation has finished
+					// this stops the panel opening empty
+					return false;
+				} else {
+					navClosing = true;
+					// wait for css animation to end
+					afterClose = setTimeout(function () {
+						navClosing = false;
+						removeClass(nav, 'nav-open');
+						navOpen = false;
+					}, 500);
+				}
 				removeClass(body, 'nav-active');
-				// removeClass(header, 'nav-active');
-				// removeClass(main, 'nav-active');
-				open = false;
+				navOpen = false;
 			} else {
 				addClass(body, 'nav-active');
-				// addClass(header, 'nav-active');
-				// addClass(main, 'nav-active');
-				open = true;
+				addClass(nav, 'nav-open');
+				navOpen = true;
 			}
 		}, false);
 	};
 	return toggle();
 })();
+
+if (Barclays.mode['gte-md']) { // ideally we'd get this as dot notation, eg Barclays.mode.gte.md
+			$nav.toggleClass('nav-open');
+		} else {
+			if (navOpen) {
+				if (navClosing) {
+					// navigation is closing, do nothing until the animation has finished
+					// this stops the panel opening empty
+					return false;
+				} else {
+					navClosing = true;
+					// wait for css animation to end
+					afterClose = setTimeout(function () {
+						navClosing = false;
+						$nav.removeClass('nav-open');
+						navOpen = false;
+					}, 500);
+				}
+			} else {
+				$nav.addClass('nav-open');
+				navOpen = true;
+			}
+		}
